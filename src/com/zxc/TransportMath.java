26f12c0ca47a249d827b02e5e4f3a98a446ee292
snake <elserpiente@rambler.ru>
2013-12-05 19:05:15 +0200

    adde new test

--- src/com/zxc/TransportMath.java
+++ src/com/zxc/TransportMath.java
@@ -1,24 +1,30 @@
 package com.zxc;
 
 import java.util.Vector;
+import com.zxc.Vector2d;
 
 /**
  * Created by snake on 05.12.13.
  */
 
-public class TransportMath {
-    public static Float GetSectionEqualA(Vector<Float> p1, Vector<Float> p2)
+ public class TransportMath {
+
+    public static Float GetSectionEqualA(Vector2d<Float> p1, Vector2d<Float> p2)
     {
-        return p2.get(1)-p1.get(1);
+        return p2.getY()-p1.getY();
 
     }
-    public static Float GetSectionEqualB(Vector<Float> p1, Vector<Float> p2)
+    public static Float GetSectionEqualB(Vector2d<Float> p1, Vector2d<Float> p2)
     {
-        return p1.get(0) - p2.get(0);
+        return p1.getX() - p2.getX();
     }
-    public static Float GetSectionEqualC(Vector<Float> p1, Vector<Float> p2)
+    public static Float GetSectionEqualC(Vector2d<Float> p1, Vector2d<Float> p2)
     {
 
-        return (p2.get(0) - p1.get(0))*p1.get(1) + p1.get(0)*(p1.get(1) - p2.get(1));
+        return (p2.getX() - p1.getX())*p1.getY() + p1.getX()*(p1.getY() - p2.getY());
+    }
+    public static Vector2d<Float> GetPointOnSection(Vector2d<Float> p, Float a, Float b, Float c)
+    {
+        return new Vector2d<Float>(0.0f,0.0f);
     }
 }
--- unit_tests/com/zxc/test/TransportMathTest.java
+++ unit_tests/com/zxc/test/TransportMathTest.java
@@ -1,6 +1,8 @@
 package com.zxc.test;
 import com.zxc.TransportMath;
 import junit.framework.TestCase;
+import com.zxc.Vector2d;
+
 
 import java.util.Vector;
 
@@ -10,21 +12,27 @@ import java.util.Vector;
 public class TransportMathTest extends TestCase {
     public void testTask1()
     {
-        Vector<Float> p1 = new Vector<Float>();
-        Vector<Float> p2 = new Vector<Float>();
-        p1.add(1.0f);
-        p1.add(1.0f);
 
-        p2.add(4.0f);
-        p2.add(2.0f);
+        Vector2d<Float> p1 = new Vector2d<Float>(1.0f, 1.0f);
+        Vector2d<Float> p2 = new Vector2d<Float>(4.0f, 2.0f);
 
 
         Float A = TransportMath.GetSectionEqualA(p1, p2);
         Float B = TransportMath.GetSectionEqualB(p1, p2);
         Float C = TransportMath.GetSectionEqualC(p1, p2);
-        assertEquals(1.0f,A);
+        assertEquals(1.0f, A);
         assertEquals(-3.0f,B);
         assertEquals(2.0f,C);
+    }
+    public void testFindPoint()
+    {
+        Float A = 4.0f;
+        Float B = -3.0f;
+        Float C = 35.0f;
+        Vector2d<Float> M = new Vector2d<Float>(-1.0f, 2.0f);
+        Vector2d<Float> H = TransportMath.GetPointOnSection(M, A, B, C);
+        assertEquals(-5.0f, H.getX() );
+        assertEquals(5.0f, H.getY() );
     }
 
     protected void setUp() throws java.lang.Exception
